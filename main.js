cfdf93fe0ae6d7ee0d94adfa21289a1668975601
jihad <mdjihadislam166@gmail.com>
2025-01-25 09:42:12 +0600

    done

--- main.js
+++ main.js
@@ -56,3 +56,24 @@ document.querySelectorAll(".faq-question").forEach((question) => {
 
 
 
+// custom cursor 
+
+
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursorOutline = document.querySelector("[data-cursor-outline]");
+
+window.addEventListener("mousemove", function (e) {
+  const posX = e.clientX;
+  const posY = e.clientY;
+
+  cursorDot.style.left = `${posX}px`;
+  cursorDot.style.top = `${posY}px`;
+
+  cursorOutline.animate(
+    [
+      { left: `${cursorOutline.style.left}`, top: `${cursorOutline.style.top}` },
+      { left: `${posX}px`, top: `${posY}px` }
+    ],
+    { duration: 500, fill: "forwards" }
+  );
+});
